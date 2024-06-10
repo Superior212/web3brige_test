bfdee4b6808b7876c256cbe12babe4232d429ec2
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-06-10 10:31:00 +0100

    chore: Add TypeScript types to SearchResults component

--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -1,5 +1,10 @@
+import React from "react";
 
-const SearchResults = ({ results }) => {
+interface SearchResultsProps {
+  results: string[];
+}
+
+const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
   return (
     <div>
       <h2>Search Results</h2>
