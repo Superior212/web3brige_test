727139169ce42e79dbb8325a00b83d391b440961
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-06-10 11:30:02 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3001;
-const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8000;
+const URI = process.env.URI;
+const mongoose = require('mongoose')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+
+
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+})
+
+const userModel = mongoose.model("User", userSchema)
+
 const getData = () => {
-  const data = fs.readFileSync("data.json");
+
   return JSON.parse(data);
 };
 
@@ -19,16 +36,16 @@ const saveData = (data) => {
 
 app.get("/search", (req, res) => {
   const query = req.query.q.toLowerCase();
-  const data = getData();
-  const results = data.filter((item) => item.toLowerCase().includes(query));
-  res.json(results.length ? results : "No results found");
+  // const data = getData();
+  // const results = data.filter((item) => item.toLowerCase().includes(query));
+  const data = userModel.find({ userName: query });
+  res.json(results.length ? data : "No results found");
 });
 
 app.post("/add", (req, res) => {
   const newItem = req.body.item;
-  const data = getData();
-  data.push(newItem);
-  saveData(data);
+  userModel.save({ userName: newItem })
+  console.log(newItem);
   res.json({ message: "Item added successfully" });
 });
 
@@ -45,6 +62,14 @@ app.put("/update", (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+
+
+mongoose.connect(URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`connected to database & Server is running on port: ${port}`);
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  })
